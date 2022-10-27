04bf5b4f6b4d7e6217d8655608a40cc03bc9e3e1
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-10-27 09:35:52 +0430

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import Calculator from './components/Calculator';
+
+export default class App extends React.PureComponent {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
