b80719e20fd7fea290f1fe74598f828d37f70413
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-10-29 10:31:55 +0430

    Created operate function

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,30 +1,33 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Big from 'big.js';
 
 export default function operate(numberOne, numberTwo, operation) {
-    const one = Big(numberOne);
-    const Two = Big(numberTwo);
-    if (operation == '+'){
-        return one.plus(two).toString();
+  const one = Big(numberOne);
+  const Two = Big(numberTwo);
+  if (operation === '+') {
+    return one.plus(Two).toString();
+  }
+  if (operation === '-') {
+    return one.minus(Two).toString();
+  }
+  if (operation === 'x') {
+    return one.times(Two).toString();
+  }
+  if (operation === '÷') {
+    try {
+      return one.div(Two).toString();
+    } catch (err) {
+      return "Can't divide by 0.";
     }
-    if (operation == '-'){
-        return one.minus(two).toString();
+  }
+
+  if (operation === '%') {
+    try {
+      return one.mod(Two).toString();
+    } catch (err) {
+      return "Can't find modulo as can't divide by 0.";
     }
-    if (operation == 'x'){
-        return one.times(two).toString();
-    }
-    if (operation == '÷'){
-        try{
-            return one.div(two).toString();
-        } catch (err) {
-            return "Can't divide by 0.";
-        }
-    }
-    if (operation == '%') {
-        try{
-            return one.mod(two).toString();
-        } catch (err) {
-            return "Can't find modulo as can't divide by 0.";
-        }
-    }
-    throw Error('Unknown operation '${operation}'`);
+  }
+  // eslint-disable-next-line linebreak-style
+  throw Error(`Unknown operation "${operation}"`);
 }
