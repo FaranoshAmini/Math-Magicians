033391c463b552bcd2c1cbef90f69e9224607f56
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-04 13:14:28 +0430

    Update Quote.js

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Quote() {
   return (
     <main>
-      <p>
+      <p className="quote">
         Mathematic is not about numbers, equations, computations, or algorithm.
-        it is about understanding. - William Paul Thurston
+        it is about understanding.   - William Paul Thurston
       </p>
     </main>
   );
