ee32e4c644b7b93cc87668533d726d3df68b04ca
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-04 10:05:21 +0430

    Import Home and Quote pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React from 'react';
 // eslint-disable-next-line import/extensions
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './Components/Home';
 import Calculator from './Components/Calculator.js';
+import Quote from './Components/Quote';
 
 export default class App extends React.PureComponent {
   render() {
     return (
-      <Calculator />
+      
     );
   }
 }
